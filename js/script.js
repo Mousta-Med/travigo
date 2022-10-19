af98363bfce33591e57bf653c8b4de4e2487c0f2
Mohammed Moustarhfir <112891663+Mousta-Med@users.noreply.github.com>
2022-10-19 14:15:28 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const header = document.querySelector("header");
+const burger = document.getElementById("menu-icon");
 
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 0);
+	burger.classList.toggle ("dark", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
